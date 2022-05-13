35fde1982b33a6d6103fa134594472b01c0b8842
dodiiier <darkserness@mail.ru>
2022-05-13 13:14:17 +0300

    решение задачи на переиспользование методов

--- src/Tasks_primitive10/Main.java
+++ src/Tasks_primitive10/Main.java
@@ -2,6 +2,15 @@ package Tasks_primitive10;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(getMinFromTwo(5, 4));
+        System.out.println(getMinFromFour(1, 5, 6, 8));
+    }
 
+    public static int getMinFromTwo(int x, int y) {
+        return Math.min(x, y);
+    }
+
+    public static int getMinFromFour(int x, int y, int m, int n) {
+        return getMinFromTwo(getMinFromTwo(x, y), getMinFromTwo(m, n));
     }
 }
