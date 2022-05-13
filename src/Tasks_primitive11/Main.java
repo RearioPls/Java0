6447484107d0b022c09f15fdc983d19e838bbbc7
dodiiier <darkserness@mail.ru>
2022-05-13 13:36:16 +0300

    решение по выводу каждого числа из массива

--- src/Tasks_primitive11/Main.java
+++ src/Tasks_primitive11/Main.java
@@ -2,6 +2,14 @@ package Tasks_primitive11;
 
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[]{2, 4, 6};
+        printArray(arr);
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.println(i);
+        }
 
     }
 }
