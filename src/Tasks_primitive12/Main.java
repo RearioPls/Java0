bd3bfc25089501694fb9c7c5bad9787de5dee040
dodiiier <darkserness@mail.ru>
2022-05-13 14:34:29 +0300

    нет решения

--- src/Tasks_primitive12/Main.java
+++ src/Tasks_primitive12/Main.java
@@ -2,6 +2,13 @@ package Tasks_primitive12;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(fillArray(7));
+    }
+    static int[] fillArray(int x){
+        int[] arr = new int[x];
+        for (int i=0;i<arr.length;i++){
+            return arr;
+        }
+        return arr;
     }
 }
