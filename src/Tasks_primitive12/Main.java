e83835dd3ba58e5ff26b2a8eaabe1aaaed8cc2fd
dodiiier <darkserness@mail.ru>
2022-05-13 14:41:30 +0300

    задача на массив

--- src/Tasks_primitive12/Main.java
+++ src/Tasks_primitive12/Main.java
@@ -1,13 +1,15 @@
 package Tasks_primitive12;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(fillArray(7));
+        System.out.println(Arrays.toString(fillArray(7)));
     }
     static int[] fillArray(int x){
         int[] arr = new int[x];
         for (int i=0;i<arr.length;i++){
-            return arr;
+            arr[i]=i;
         }
         return arr;
     }
