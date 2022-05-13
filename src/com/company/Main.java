10fdd02e247f911e65785aed944952f3ded952ad
dobroseriii <darkserness@mail.ru>
2022-05-13 11:11:24 +0300

    первая задача

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	String name = "Serega";
+        System.out.println(name);
     }
 }
